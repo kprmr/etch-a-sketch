78c3b24a930cc330b269254ebba3aa922edc65f8
Kushagra Parmar <parmarskush@gmail.com>
2025-09-05 18:17:07 +0530

    Made the grid prototype
    The innner boxes will change color to yellow when hovered over it, but the dimensions/total number of pixels are different for every new value solving this sould be next step.

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 16*16; i++) {
-    const content = document.createElement("div");
-    content.classList.add("content");
-    // content.textContent = "Hello"
-    container.appendChild(content);
+const value = prompt("What is the size of box")
+function grid(a){
+    for (let i = 0; i < a*a; i++) {
+        const content = document.createElement("div");
+        content.classList.add("content");
+        // content.textContent = "Hello"
+        container.appendChild(content);
+        content.addEventListener('mouseover', function(){
+        content.style.backgroundColor = 'yellow'
+        })
+    }
 }
+grid(value)
+// const btn = document.querySelector("#btn");
+// btn.addEventListener('click', () => {
+//     
+//     grid(value)
+// })
+const btn = document.querySelector("#btn")
+btn.addEventListener('click', () => {
+    window.location.reload()
+})
+
+
