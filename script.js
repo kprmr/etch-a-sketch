dd0b50730202ab457850831efee70a49a9cf1d6e
Kushagra Parmar <parmarskush@gmail.com>
2025-09-08 16:44:49 +0530

    Adjusted the size of inner boxes
    The size of inner boxes now change with the number inputed and the colors are also randomized when mouse hover over them which was edited in script.js and changed the background color of page and button in style.css.

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 const container = document.querySelector("#container");
 const value = prompt("What is the size of box")
+
+//The line below is one line code to generate random color
+// var newRandomColor = Math.floor(Math.random()*16777215).toString(16)
+
+
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+//This function will make the grid
 function grid(a){
     for (let i = 0; i < a*a; i++) {
         const content = document.createElement("div");
         content.classList.add("content");
         // content.textContent = "Hello"
+        const newColor = randomColor()
         container.appendChild(content);
+        content.style.flexBasis = (800 / a) + "px";
+        content.style.height = (800 / a) + "px";
         content.addEventListener('mouseover', function(){
-        content.style.backgroundColor = 'yellow'
+            // setInterval(() => {
+            //     content.style.backgroundColor = newColor;
+            // }, 1000);
+            content.style.backgroundColor = newColor
         })
     }
 }
 grid(value)
-// const btn = document.querySelector("#btn");
-// btn.addEventListener('click', () => {
-//     
-//     grid(value)
-// })
+
 const btn = document.querySelector("#btn")
 btn.addEventListener('click', () => {
     window.location.reload()
 })
+
 
 
